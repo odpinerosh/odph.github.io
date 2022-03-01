213dc6189db1e28fcca306daa2f783884fc47b59
odph <odpinerosh@gmail.com>
2022-03-01 14:55:32 -0500

    fix: fix className in FeTech.js

--- src/components/WhoIs/FeTech.js
+++ src/components/WhoIs/FeTech.js
@@ -11,10 +11,10 @@ const FeTech = () => {
         <div className = 'fe-tech'>
             <div className="img-container">
                 <ul className="img-list">
-                    <li><i class="fab fa-react esp-react"></i></li>
-                    <li><i class="fab fa-html5"></i></li>
-                    <li><i class="fab fa-css3"></i></li>
-                    <li><i class="fab fa-js"></i></li>
+                    <li><i className="fab fa-react esp-react"></i></li>
+                    <li><i className="fab fa-html5"></i></li>
+                    <li><i className="fab fa-css3"></i></li>
+                    <li><i className="fab fa-js"></i></li>
                 </ul>
             </div>
         </div>
