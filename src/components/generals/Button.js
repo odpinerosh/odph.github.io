83ceb2f82297b92d38ca1b133f7c54b09a66c7d2
odph <odpinerosh@gmail.com>
2022-03-02 16:05:33 -0500

    fix: bug in Button.js

--- src/components/generals/Button.js
+++ src/components/generals/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 const Button = (props) => {
 
     return (
-        <a className="btn-visit" href={props.url} target='_blank'>
+        <a className="btn-visit" href={props.url} target='_blank' rel="noreferrer">
             {props.caption}
         </a>
     );
