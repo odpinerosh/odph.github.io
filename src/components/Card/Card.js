661ad49983733468ffd23ed05dee5646fb07585b
odph <odpinerosh@gmail.com>
2022-03-08 18:03:49 -0500

    implement css clamp()

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -6,14 +6,12 @@ const Card = (props) => {
 
     return (
         <div className="single-card">
+            <h3 className="pro-title">{props.pname}</h3>
             <div className = "card-img">
                 <img src={props.pimg} alt={props.pname} />
             </div>
-            <div className="card-body">
-                <h3 className="pro-title">{props.pname}</h3>
-                <p className="pro-desc">{props.ptext}</p>
-                <Button caption="Visitar" url={props.purl}/>
-            </div>
+            <p className="pro-desc">{props.ptext}</p>
+            <Button caption="Visitar" url={props.purl}/>
         </div>
     );
 }
