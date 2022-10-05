0b3a96734b34c6b3401e38803c7ce8defc959cbf
odpinerosh <odpinerosh@gmail.com>
2022-10-05 15:36:10 -0500

    Updates

---
+++ precache-manifest.fb7073a9317ab7ade36febddd1de00bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aff934498614be6ed4b",
+    "url": "/static/css/main.706f9c81.chunk.css"
+  },
+  {
+    "revision": "5aff934498614be6ed4b",
+    "url": "/static/js/main.5aff9344.chunk.js"
+  },
+  {
+    "revision": "0757bc4a0676d4258441",
+    "url": "/static/js/1.0757bc4a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8041f04b6b05127ba67549dca62ab73",
+    "url": "/static/media/heroimg.d8041f04.png"
+  },
+  {
+    "revision": "248b746a803ba7397d87e9f012c4075f",
+    "url": "/index.html"
+  }
+];
