49b592774f129865a741c60ffd91f635e8ca9526
odpinerosh <odpinerosh@gmail.com>
2023-07-07 19:59:09 -0500

    fix: broken link to linkedin

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
             <h2 className="contact-title">contacto</h2>
             <ul className="contact-list">
                 <li className="contact-list-li"><a href="https://twitter.com/odpinerosh" target="_blank" rel="noreferrer"><i className="fab fa-twitter"></i></a></li>
-                <li className="contact-list-li"><a href="https://www.linkedin.com/in/odph/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a></li>
+                <li className="contact-list-li"><a href="https://co.linkedin.com/in/odph" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a></li>
                 <li className="contact-list-li"><a href="https://github.com/odpinerosh/" target="_blank" rel="noreferrer"><i className="fab fa-github"></i></a></li>
             </ul>
         </section>
